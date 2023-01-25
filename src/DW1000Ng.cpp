783908de74f089ecf8cb699a380c31b6c338fe0e
안현기 <54882000+tykr0001@users.noreply.github.com>
2023-01-25 19:47:59 +0900

    update

--- src/DW1000Ng.cpp
+++ src/DW1000Ng.cpp
@@ -25,7 +25,7 @@
 /*
  * Copyright (c) 2015 by Thomas Trojer <thomas@trojer.net>
  * Decawave DW1000Ng library for arduino.
- *
+ *urrentChannel
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
---
+++ src/StandardRTLSAnchorMainWiFi_TWR/StandardRTLSAnchorMainWiFi_TWR.ino
@@ -0,0 +1,240 @@
+#include <DW1000Ng.hpp>
+#include <DW1000NgUtils.hpp>
+#include <DW1000NgRanging.hpp>
+#include <DW1000NgRTLS.hpp>
+#include <WiFi.h>
+
+const char* ssid     = "yourssid";
+const char* password = "yourpasswd";
+
+WiFiServer server(80);
+
+int value = 0;
+
+typedef struct Position {
+    double x;
+    double y;
+} Position;
+
+// connection pins
+const uint8_t PIN_SCK = 18;  
+const uint8_t PIN_MOSI = 23; 
+const uint8_t PIN_MISO = 19;  
+const uint8_t PIN_SS = 2;  
+const uint8_t PIN_RST = 15;  
+const uint8_t PIN_IRQ = 17;  
+
+// Extended Unique Identifier register. 64-bit device identifier. Register file: 0x01
+char EUI[] = "AA:BB:CC:DD:EE:FF:00:01";
+
+Position position_self = {0,0};
+Position position_B = {3,0};
+Position position_C = {3,2.5};
+
+double range_self;
+double range_B;
+double range_C;
+double range_D;
+
+boolean received_B = false;
+boolean received_C = false;
+boolean received_D = false;
+
+byte target_eui[8];
+byte tag_shortAddress[] = {0x05, 0x00};
+
+byte anchor_b[] = {0x02, 0x00};
+byte anchor_c[] = {0x03, 0x00};
+byte anchor_d[] = {0x04, 0x00};
+
+uint16_t next_anchor = 2;
+
+device_configuration_t DEFAULT_CONFIG = {
+    false,
+    true,
+    true,
+    true,
+    false,
+    SFDMode::STANDARD_SFD,
+    Channel::CHANNEL_5,
+    DataRate::RATE_850KBPS,
+    PulseFrequency::FREQ_16MHZ,
+    PreambleLength::LEN_256,
+    PreambleCode::CODE_3
+};
+
+frame_filtering_configuration_t ANCHOR_FRAME_FILTER_CONFIG = {
+    false,
+    false,
+    true,
+    false,
+    false,
+    false,
+    false,
+    true /* This allows blink frames */
+};
+
+void setup() {
+    // DEBUG monitoring
+    Serial.begin(115200);
+    Serial.println(F("### DW1000Ng-arduino-ranging-anchorMain ###"));
+    // initialize the driver
+    #if defined(ESP8266)
+    DW1000Ng::initializeNoInterrupt(PIN_SS);
+    #else
+    DW1000Ng::initializeNoInterrupt(PIN_SS, PIN_RST);
+    #endif
+    Serial.println(F("DW1000Ng initialized ..."));
+    // general configuration
+    DW1000Ng::applyConfiguration(DEFAULT_CONFIG);
+    DW1000Ng::enableFrameFiltering(ANCHOR_FRAME_FILTER_CONFIG);
+    
+    DW1000Ng::setEUI(EUI);
+
+    DW1000Ng::setPreambleDetectionTimeout(64);
+    DW1000Ng::setSfdDetectionTimeout(273);
+    DW1000Ng::setReceiveFrameWaitTimeoutPeriod(5000);
+
+    DW1000Ng::setNetworkId(RTLS_APP_ID);
+    DW1000Ng::setDeviceAddress(1);
+  
+    DW1000Ng::setAntennaDelay(16436);
+    
+    Serial.println(F("Committed configuration ..."));
+    // DEBUG chip info and registers pretty printed
+    char msg[128];
+    DW1000Ng::getPrintableDeviceIdentifier(msg);
+    Serial.print("Device ID: "); Serial.println(msg);
+    DW1000Ng::getPrintableExtendedUniqueIdentifier(msg);
+    Serial.print("Unique ID: "); Serial.println(msg);
+    DW1000Ng::getPrintableNetworkIdAndShortAddress(msg);
+    Serial.print("Network ID & Device Address: "); Serial.println(msg);
+    DW1000Ng::getPrintableDeviceMode(msg);
+    Serial.print("Device mode: "); Serial.println(msg);    
+
+    WiFi.begin();
+     while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.println("WiFi not connected");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    server.begin();
+}
+
+/* using https://math.stackexchange.com/questions/884807/find-x-location-using-3-known-x-y-location-using-trilateration */
+void calculatePosition(double &x, double &y) {
+
+    /* This gives for granted that the z plane is the same for anchor and tags */
+    double A = ( (-2*position_self.x) + (2*position_B.x) );
+    double B = ( (-2*position_self.y) + (2*position_B.y) );
+    double C = (range_self*range_self) - (range_B*range_B) - (position_self.x*position_self.x) + (position_B.x*position_B.x) - (position_self.y*position_self.y) + (position_B.y*position_B.y);
+    double D = ( (-2*position_B.x) + (2*position_C.x) );
+    double E = ( (-2*position_B.y) + (2*position_C.y) );
+    double F = (range_B*range_B) - (range_C*range_C) - (position_B.x*position_B.x) + (position_C.x*position_C.x) - (position_B.y*position_B.y) + (position_C.y*position_C.y);
+
+    x = (C*E-F*B) / (E*A-B*D);
+    y = (C*D-A*F) / (B*D-A*E);
+}
+
+void loop() {
+    // region: WiFi
+    WiFiClient client = server.available();   // listen for incoming clients
+    if (client) {                             // if you get a client,
+        Serial.println("New Client.");           // print a message out the serial port
+        String currentLine = "";                // make a String to hold incoming data from the client
+        while (client.connected()) {            // loop while the client's connected
+            if (client.available()) {             // if there's bytes to read from the client,
+                char c = client.read();             // read a byte, then
+                Serial.write(c);                    // print it out the serial monitor
+                if (c == '\n') {                    // if the byte is a newline character
+                    // if the current line is blank, you got two newline characters in a row.
+                    // that's the end of the client HTTP request, so send a response:
+                    if (currentLine.length() == 0) {
+                        // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+                        // and a content-type so the client knows what's coming, then a blank line:
+                        client.println("HTTP/1.1 200 OK");
+                        client.println("Content-type:text/html");
+                        client.println();
+    
+                        // the content of the HTTP response follows the header:
+                        client.print("Click <a href=\"/H\">here</a> to turn the LED on pin 5 on.<br>");
+                        client.print("Click <a href=\"/L\">here</a> to turn the LED on pin 5 off.<br>");
+            
+                        // The HTTP response ends with another blank line:
+                        client.println();
+                        // break out of the while loop:
+                        break;
+                      } else {    // if you got a newline, then clear currentLine:
+                          currentLine = "";
+                      }
+                } else if (c != '\r') {  // if you got anything else but a carriage return character,
+                    currentLine += c;      // add it to the end of the currentLine
+                }
+    
+                // Check to see if the client request was "GET /H" or "GET /L":
+                if (currentLine.endsWith("GET /H")) {
+                  digitalWrite(5, HIGH);               // GET /H turns the LED on
+                }
+                if (currentLine.endsWith("GET /L")) {
+                  digitalWrite(5, LOW);                // GET /L turns the LED off
+                }
+            }
+        }
+        // close the connection:
+        client.stop();
+        Serial.println("Client Disconnected.");
+    }
+
+    // region: UWB
+    if(DW1000NgRTLS::receiveFrame()){
+        size_t recv_len = DW1000Ng::getReceivedDataLength();
+        byte recv_data[recv_len];
+        DW1000Ng::getReceivedData(recv_data, recv_len);
+
+
+        if(recv_data[0] == BLINK) {
+            DW1000NgRTLS::transmitRangingInitiation(&recv_data[2], tag_shortAddress);
+            DW1000NgRTLS::waitForTransmission();
+
+            RangeAcceptResult result = DW1000NgRTLS::anchorRangeAccept(NextActivity::RANGING_CONFIRM, next_anchor);
+            if(!result.success) return;
+            range_self = result.range;
+
+            String rangeString = "Range: "; rangeString += range_self; rangeString += " m";
+            rangeString += "\t RX power: "; rangeString += DW1000Ng::getReceivePower(); rangeString += " dBm";
+            Serial.println(rangeString);
+
+        } else if(recv_data[9] == 0x60) {
+            double range = static_cast<double>(DW1000NgUtils::bytesAsValue(&recv_data[10],2) / 1000.0);
+            String rangeReportString = "Range from: "; rangeReportString += recv_data[7];
+            rangeReportString += " = "; rangeReportString += range;
+            Serial.println(rangeReportString);
+            if(recv_data[7] == anchor_b[0] && recv_data[8] == anchor_b[1]) {
+                range_B = range;
+                received_B = true;
+            } else if(recv_data[7] == anchor_c[0] && recv_data[8] == anchor_c[1]){
+                range_C = range;
+                received_C = true;
+            } else if(recv_data[7] == anchor_d[0] && recv_data[8] == anchor_d[1]){
+                range_D = range;
+                received_D = true;
+            }
+            if (received_B == true && received_C == true) {
+                double x,y;
+                calculatePosition(x,y);
+                String positioning = "Found position - x: ";
+                positioning += x; positioning +=" y: ";
+                positioning += y;
+                Serial.println(positioning);
+                received_B = false;
+                received_C = false;
+                received_D = false;
+            }
+        }
+    }
+}
